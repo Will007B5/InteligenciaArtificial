b9122c9bcac16408b801924c54b45a3c9383d780
Guillermo Andrés Sámano López <s14120027@alumnos.itsur.edu.mx>
2018-03-20 22:26:23 -0600

    Actualizacion2

---
+++ productoInterno/productoInterno.py
@@ -0,0 +1,13 @@
+vA = [-5, 4]
+vB = [-3, -8]
+
+multiA = (vA[0] * vB[0])
+multiB = (vA[1] * vB[1])
+
+resul = multiA + multiB
+
+print("u= ", vA)
+print("v= ", vB)
+print("u · v= (",vA[0]," * ",vB[0],") + (",vA[1]," * ",vB[1],")")
+print("u · v= (",multiA,") + (",multiB,")")
+print("u · v= ",resul)
